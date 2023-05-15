eae28d3ee607fa099763a182736b54caffecbb8b
Nurlana Huseyinova <80hd0r0@code.edu.az>
2023-05-15 10:12:19 +0400

    feat: quickly landing page

--- 21.Page/app.js
+++ 21.Page/app.js
@@ -1,23 +1,20 @@
-let head=document.querySelector("#head")
-let bar=document.querySelector("#bar")
-let p=document.querySelector("#paragraph")
-let headUl=document.querySelector("#headUl")
-let headNav=document.querySelector("#headnav")
-let headLiA=document.querySelectorAll(".lia")
+let head = document.querySelector("#head");
+let bar = document.querySelector("#bar");
+let p = document.querySelector("#paragraph");
+let headUl = document.querySelector("#headUl");
 
-
-bar.addEventListener("click",function(){
-        head.style.backgroundColor="black"
-        head.style.backgroundImage="none"
-        p.style.display="none"
-        headUl.style.display="block"
-        headUl.style.paddingInlineStart="0px"
-})
-
-
-
-
-
-
-
-
+let click = false;
+bar.addEventListener("click", function () {
+  click = !click;
+  if (click) {
+    head.style.backgroundColor = "black";
+    head.style.backgroundImage = "none";
+    p.style.display = "none";
+    headUl.style.display = "block";
+  } else {
+    head.style.backgroundColor = "none";
+    head.style.backgroundImage = "url('../desktopimg/hero.jpg')";
+    p.style.display = "block";
+    headUl.style.display = "none";
+  }
+});
